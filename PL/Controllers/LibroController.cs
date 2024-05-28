ed4ee3763ba2ae8888f9d5ed8d50060aecfa46d7
manuelflores99 <manuelflores.h99@gmail.com>
2024-05-28 14:44:51 -0600

    Add controller about Libro

---
+++ PL/Controllers/LibroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PL.Controllers
+{
+    public class LibroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
