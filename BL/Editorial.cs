7f4791f16f0c93c26881ab2fe76a29eb7a1c1e57
Lizy0905 <pamelaelizabethvillareyes@gmail.com>
2024-05-27 19:16:50 -0600

    Add Prop ML.Ciudad

--- BL/Editorial.cs
+++ BL/Editorial.cs
@@ -8,6 +8,38 @@ namespace BL
 {
     public class Editorial
     {
+        //public static (bool success, string message, ML.Editorial, Exception ex) GetAll()
+        //{
+        //    ML.Editorial editorial = new ML.Editorial();
+        //    try
+        //    {
+        //        using (DL.AppDbContext context = new DL.AppDbContext())
+        //        {
+        //            var query = (from editorials in context.Editorials
 
+        //                         select new
+        //                         {
+        //                             IdEditorial = editorials.IdEditorial,
+        //                             Nombre = editorials.Nombre,
+        //                             IdCiudad = editorials.IdCity
+        //                         }).ToList();
+
+        //            if (query != null)
+        //            {
+
+
+        //            }
+
+
+        //        }
+
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //    }
+
+        //}
     }
 }
