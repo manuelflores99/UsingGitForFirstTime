c56d99c44173c92e7b28778125f6d03d254bf45a
manuelflores99 <manuelflores.h99@gmail.com>
2024-05-27 18:39:32 -0600

    Add DL with Entities

---
+++ DL/AppDbContext.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DL;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Editorial> Editorials { get; set; }
+
+    public virtual DbSet<Libro> Libros { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=.; Database=UsingGitForFirstTime; TrustServerCertificate=True; User ID=sa; Password=pass@word1;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e => e.IdCity).HasName("PK__City__394B023AA59E208B");
+
+            entity.ToTable("City");
+
+            entity.Property(e => e.Nombre)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Editorial>(entity =>
+        {
+            entity.HasKey(e => e.IdEditorial).HasName("PK__Editoria__EF838671321ED377");
+
+            entity.ToTable("Editorial");
+
+            entity.Property(e => e.Nombre)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdCityNavigation).WithMany(p => p.Editorials)
+                .HasForeignKey(d => d.IdCity)
+                .HasConstraintName("FK__Editorial__IdCit__1273C1CD");
+        });
+
+        modelBuilder.Entity<Libro>(entity =>
+        {
+            entity.HasKey(e => e.IdLibro).HasName("PK__Libro__3E0B49AD99B908F4");
+
+            entity.ToTable("Libro");
+
+            entity.Property(e => e.AnioPublicacion).HasColumnType("date");
+            entity.Property(e => e.Autor)
+                .IsRequired()
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Isbn)
+                .IsRequired()
+                .HasMaxLength(30)
+                .IsUnicode(false)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Titulo)
+                .IsRequired()
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdEditorialNavigation).WithMany(p => p.Libros)
+                .HasForeignKey(d => d.IdEditorial)
+                .HasConstraintName("FK__Libro__IdEditori__15502E78");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DL/Editorial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DL;
+
+public partial class Editorial
+{
+    public int IdEditorial { get; set; }
+
+    public string Nombre { get; set; }
+
+    public int? IdCity { get; set; }
+
+    public virtual City IdCityNavigation { get; set; }
+
+    public virtual ICollection<Libro> Libros { get; set; } = new List<Libro>();
+}
